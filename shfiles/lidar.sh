0a6fd32c5ce3ed0e62cadcb253509362c2caf69f
XJY <jiayi_xie_xjtu@outlook.com>
2025-12-02 12:10:41 +0800

    now it is ros version

--- shfiles/lidar.sh
+++ shfiles/lidar.sh
@@ -4,5 +4,3 @@ sleep 3s
 gnome-terminal -t "fastlio" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch fast_lio mapping_mid360.launch;exec bash"
 sleep 5s
 gnome-terminal -t "lite3_deploy_pkgs" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch transform_odom transform_odom.launch;exec bash"
-sleep 2s
-gnome-terminal -t "lite3_udp" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && rosrun lite3_ego_udp lite3_ego_udp_node;exec bash"
