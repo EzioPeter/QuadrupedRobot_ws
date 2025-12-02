82937c815473daa6bfe2ec9565aab83fa073f459
XJY <jiayi_xie_xjtu@outlook.com>
2025-12-02 12:21:35 +0800

    add quick start shell file start.sh

---
+++ shfiles/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gnome-terminal -t "lidarmsg" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch livox_ros_driver2 msg_MID360.launch;exec bash;"
+sleep 3s
+gnome-terminal -t "fastlio" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch fast_lio mapping_mid360.launch;exec bash"
+sleep 5s
+gnome-terminal -t "transform_odom" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch transform_odom transform_odom.launch;exec bash"
+sleep 3s
+gnome-terminal -t "lite3_ros" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch message_transformer message_transformer.launch;exec bash"
+sleep 3s
+gnome-terminal -t "ego" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch ego_planner run_in_lite3.launch;exec bash;"
