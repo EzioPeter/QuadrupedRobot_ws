80f456f0827e7bbd5a73043a14e72aca6bb86561
XJY <jiayi_xie_xjtu@outlook.com>
2025-12-06 16:05:37 +0800

    update lidar.sh and find that when pitch angle is 15 degree, the fastlio also has 15 degree pitch offset

--- shfiles/lidar.sh
+++ shfiles/lidar.sh
@@ -2,5 +2,3 @@
 gnome-terminal -t "lidarmsg" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch livox_ros_driver2 msg_MID360.launch;exec bash;"
 sleep 3s
 gnome-terminal -t "fastlio" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch fast_lio mapping_mid360.launch;exec bash"
-sleep 5s
-gnome-terminal -t "lite3_deploy_pkgs" -- bash -c "source /home/agx002/QuadrupedRobot_ws/devel/setup.bash && roslaunch transform_odom transform_odom.launch;exec bash"
